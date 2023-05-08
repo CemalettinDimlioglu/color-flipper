a851132259265e2d1f967cf712d7e39d5ab1d8a9
CMLTTN <cemaldim@gmail.com>
2023-05-08 22:41:56 +0300

    done

--- hex.js
+++ hex.js
@@ -1,3 +1,16 @@
-const hex = [0,1,2,3,4,5,6,7,8,9, "A","B","C","D","E","F","G"]
+const hex = [0,1,2,3,4,5,6,7,8,9, "A","B","C","D","E","F"] // 1.aşama
 
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn"); // 2.aşama
+const color = document.querySelector(".color"); //.3.aşama
+
+btn.addEventListener("click",function(){ //4.aşama
+     let hexColor = "#";
+     for(let i =0; i<6;i++){
+          hexColor += hex[randomNumber()]
+     }
+     color.textContent = hexColor; //5.aşama
+     document.body.style.backgroundColor = hexColor;
+})
+function randomNumber(){   //6.aşama
+     return Math.floor(Math.random()*hex.length)
+}
